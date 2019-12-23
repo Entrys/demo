2f63397567426f4eba8eabe456074cf6b6f3d061
Li.t <Yw2017>
2019-12-23 17:26:37 +0800

    add: 重新构建项目

---
+++ config-server/src/main/java/com.yanwei.movie/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.yanwei.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author li.tao
+ * @since 2019/12/9 16:37
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
