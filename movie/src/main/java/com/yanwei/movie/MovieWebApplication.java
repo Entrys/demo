a12fe90f9d039c555814656a9a0320d2c5554e7e
Li.t <Yw2017>
2019-12-24 15:32:40 +0800

    add: 这样那样的

--- movie/src/main/java/com/yanwei/movie/MovieWebApplication.java
+++ movie/src/main/java/com/yanwei/movie/MovieWebApplication.java
@@ -3,6 +3,7 @@ package com.yanwei.movie;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication(scanBasePackages = "com.yanwei.movie")
 @MapperScan("com.yanwei.movie.repository")
+@EnableEurekaClient
 public class MovieWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieWebApplication.class, args);
